3d359333caadad16d00cd9a45700209178606f9a
unknown <adol3212@naver.com>
2021-04-27 22:35:11 +0900

    GolangStudy

--- Golang/lissajous/main.go
+++ Golang/lissajous/main.go
@@ -10,11 +10,14 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}, color.RGBA{0xff, 0x00, 0x00, 0xff}, color.RGBA{0x00, 0x00, 0xff, 0xff}, color.White}
 
 const (
-	whiteIndex = 0 // 팔레트의 첫 번째 색상
-	blackIndex = 1 // 팔레트의 다음 색상
+	blackIndex = 0 // 팔레트의 첫 번째 색상
+	greenIndex = 1 // 팔레트의 다음 색상
+	redIndex   = 2
+	blueIndex  = 3
+	whiteIndex = 4
 )
 
 func main() {
@@ -29,7 +32,6 @@ func lissajous(out io.Writer) {
 		nframes = 64    // 애니메이션 프레임 수
 		delay   = 8     // 10ms 단위의 프레임 간 지연
 	)
-
 	freq := rand.Float64() * 3.0 //y 진동자의 상대적 진동수
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 //위상차이
@@ -39,7 +41,19 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			if t < 2*math.Pi {
+				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+			}
+			if t > 2*math.Pi && t < 3*2*math.Pi {
+				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), redIndex)
+			}
+			if t > 3*math.Pi && t < 4*2*math.Pi {
+				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blueIndex)
+			}
+			if t > 4*math.Pi {
+				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), whiteIndex)
+			}
+
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
